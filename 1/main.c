c717d7c64f9b7649d71c5e5808b89eccf1adcb90
William Hammond <william.t.hammond@gmail.com>
2021-12-02 19:48:02 -0500

    Rename 1-1 to 1

--- 1/main.c
+++ 1/main.c
@@ -1,45 +1,63 @@
 #if WIN32
-    #define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_DEPRECATE
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
 #define MAX_LINE_LENGTH 1024
 
-int main(int argc, char** argv)
-{
-    char* path = "input.txt";
-    char line[MAX_LINE_LENGTH] = { 0 };
-    unsigned int line_count = 0;
+int main(int argc, char** argv) {
+  char* path = "input.txt";
+  char line[MAX_LINE_LENGTH] = {0};
+  unsigned int line_count = 0;
 
-    FILE* file = fopen(path, "r");
+  FILE* file = fopen(path, "r");
 
-    if (!file) {
-        perror(path);
-        return EXIT_FAILURE;
+  if (!file) {
+    perror(path);
+    return EXIT_FAILURE;
+  }
+
+  int bottom = NULL;
+  int middle = NULL;
+  int top = NULL;
+
+  int previousSum = NULL;
+  int currentSum = 0;
+  int result = 0;
+
+  int counter = 1;
+  while (fgets(line, MAX_LINE_LENGTH, file)) {
+    int current = atoi(line);
+    switch (counter % 3) {
+      case 0:
+        top = current;
+        break;
+      case 1:
+        bottom = current;
+        break;
+      case 2:
+        middle = current;
+        break;
+      default:
+        break;
     }
-
-    int prev = NULL;
-    int result = 0;
-    while (fgets(line, MAX_LINE_LENGTH, file)) {
-        int current = atoi(line);
-        if (!prev) {
-            prev = current;
-            continue;
-        }
-        if (current > prev) {
-            result += 1;
-        }
-        prev = current;
+    if (top && middle && bottom) {
+      currentSum = bottom + middle + top;
+      if (previousSum && currentSum > previousSum) {
+        result++;
+      }
+      previousSum = currentSum;
     }
+    counter++;
+  }
 
-    if (fclose(file)) {
-        return EXIT_FAILURE;
-        perror(path);
-    }
-    printf("result :%d", result);
-    return EXIT_SUCCESS;
+  if (fclose(file)) {
+    return EXIT_FAILURE;
+    perror(path);
+  }
+  printf("result :%d", result);
+  return EXIT_SUCCESS;
 }
