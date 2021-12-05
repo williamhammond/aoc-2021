6d71de5a7e1091e3c3d4d6a9fd3e88988f69e4c6
William Hammond <william.t.hammond@gmail.com>
2021-12-05 15:55:56 -0500

    Day 5 part 1 working

--- 5/main.c
+++ 5/main.c
@@ -8,7 +8,7 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 1024
-#define VENT_COUNT 10
+#define VENT_COUNT 500
 
 typedef struct point {
   int x;
@@ -21,7 +21,7 @@ typedef struct line {
 } line;
 
 int main(int argc, char** argv) {
-  char* path = "test.txt";
+  char* path = "input.txt";
   char line[MAX_LINE_LENGTH] = {0};
   FILE* file = fopen(path, "r");
 
@@ -38,7 +38,7 @@ int main(int argc, char** argv) {
     int x1, y1, x2, y2 = 0;
     int read = sscanf(line, "%d,%d -> %d,%d", &x1, &y1, &x2, &y2);
     if (read != 4) {
-      printf("Read %d from line", read);
+      // printf("Read %d from line", read);
       return EXIT_FAILURE;
     }
     if (maxX < x1) {
@@ -73,38 +73,46 @@ int main(int argc, char** argv) {
     ventCount++;
   }
 
-  int grid[512][512] = {};
+  int grid[1024][1024] = {};
   int dangerousTiles = 0;
 
-  for (int i = 0; i < ventCount; i++) {
-    puts("X coords match");
-    if (vents[].start.x == vents[ventCount].end.x) {
-      int lower = (vents[ventCount].start.x < vents[ventCount].end.x)
-                      ? vents[ventCount].start.x
-                      : vents[ventCount].end.x;
-      int upper = (vents[ventCount].start.x > vents[ventCount].end.x)
-                      ? vents[ventCount].start.x
-                      : vents[ventCount].end.x;
+  for (int ventIdx = 0; ventIdx < ventCount; ventIdx++) {
+    if (vents[ventIdx].start.x == vents[ventIdx].end.x) {
+      // printLine(vents[ventIdx]);
+      int lower = (vents[ventIdx].start.y < vents[ventIdx].end.y)
+                      ? vents[ventIdx].start.y
+                      : vents[ventIdx].end.y;
+      int upper = (vents[ventIdx].start.y > vents[ventIdx].end.y)
+                      ? vents[ventIdx].start.y
+                      : vents[ventIdx].end.y;
       for (int i = lower; i <= upper; i++) {
-        grid[vents[ventCount].start.x][i]++;
+        // printf("Increasing x=%d y=%d from %d to %d\n", vents[ventIdx].start.x,
+        //        i, grid[vents[ventIdx].start.x][i],
+        //        grid[vents[ventIdx].start.x][i] + 1);
+        grid[vents[ventIdx].start.x][i]++;
       }
+      // printGrid(grid, maxX, maxY);
 
-    } else if (vents[ventCount].start.y == vents[ventCount].end.y) {
-      int lower = (vents[ventCount].start.y < vents[ventCount].end.y)
-                      ? vents[ventCount].start.y
-                      : vents[ventCount].end.y;
-      int upper = (vents[ventCount].start.y > vents[ventCount].end.y)
-                      ? vents[ventCount].start.y
-                      : vents[ventCount].end.y;
+    } else if (vents[ventIdx].start.y == vents[ventIdx].end.y) {
+      // printLine(vents[ventIdx]);
+      int lower = (vents[ventIdx].start.x < vents[ventIdx].end.x)
+                      ? vents[ventIdx].start.x
+                      : vents[ventIdx].end.x;
+      int upper = (vents[ventIdx].start.x > vents[ventIdx].end.x)
+                      ? vents[ventIdx].start.x
+                      : vents[ventIdx].end.x;
       for (int i = lower; i <= upper; i++) {
-        grid[i][vents[ventCount].start.x]++;
+        // printf("Increasing x=%d y=%d from %d to %d\n", i,
+        //        vents[ventIdx].start.y, grid[i][vents[ventIdx].start.y],
+        //        grid[i][vents[ventIdx].start.y] + 1);
+        grid[i][vents[ventIdx].start.y]++;
       }
+      // printGrid(grid, maxX, maxY);
     }
-    printGrid(grid, maxX, maxY);
   }
 
-  for (int i = 0; i < maxX; i++) {
-    for (int j = 0; j < maxY; j++) {
+  for (int i = 0; i <= maxX; i++) {
+    for (int j = 0; j <= maxY; j++) {
       if (grid[i][j] > 1) {
         dangerousTiles++;
       }
@@ -122,7 +130,7 @@ int main(int argc, char** argv) {
 }
 
 void printLine(line line) {
-  printf("(%d, %d) -> (%d, %d)", line.start.x, line.start.y, line.end.x,
+  printf("(%d, %d) -> (%d, %d)\n", line.start.x, line.start.y, line.end.x,
          line.end.y);
 }
 
@@ -130,8 +138,8 @@ void printPoint(point point) { printf("(%d, %d)", point.x, point.y); }
 
 void printGrid(int grid[512][512], int maxX, int maxY) {
   puts("");
-  for (int i = 0; i < maxX; i++) {
-    for (int j = 0; j < maxY; j++) {
+  for (int i = 0; i <= maxX; i++) {
+    for (int j = 0; j <= maxY; j++) {
       printf("%d ", grid[i][j]);
     }
     puts("");
