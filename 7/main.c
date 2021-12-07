942387fa4fc4bf279fea2a0fb642719cd0ef380e
William Hammond <william.t.hammond@gmail.com>
2021-12-07 00:45:53 -0500

    Working day 7 part 2

--- 7/main.c
+++ 7/main.c
@@ -8,7 +8,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_LENGTH 1024
+#define MAX_CRAB_COUNT 2048
+#define MAX_LINE_LENGTH 8192
 
 int main(int argc, char** argv) {
   char* path = "input.txt";
@@ -21,8 +22,37 @@ int main(int argc, char** argv) {
   }
 
   char line[MAX_LINE_LENGTH] = {0};
+  int crabsPositions[MAX_CRAB_COUNT] = {0};
+  int crabCount = 0;
   if (fgets(line, MAX_LINE_LENGTH, file)) {
+    line[strcspn(line, "\n")] = 0;
+    char* token = strtok(line, ",");
+    while (token) {
+      int position = atoi(token);
+      crabsPositions[crabCount] = position;
+      token = strtok(NULL, ",");
+      crabCount++;
+    }
   }
+
+  int differences[MAX_CRAB_COUNT][MAX_CRAB_COUNT] = {0};
+  for (int i = 0; i < crabCount - 1; i++) {
+    for (int j = i + 1; j < crabCount; j++) {
+      differences[i][j] = abs(crabsPositions[i] - crabsPositions[j]);
+      differences[j][i] = abs(crabsPositions[i] - crabsPositions[j]);
+    }
+  }
+  int answer = INT_MAX;
+  for (int i = 0; i < crabCount; i++) {
+    int sum = 0;
+    for (int j = 0; j < crabCount; j++) {
+      sum += (differences[j][i]);
+    }
+    if (sum < answer) {
+      answer = sum;
+    }
+  }
+  printf("Answer: %d\n", answer);
 
   if (fclose(file)) {
     return EXIT_FAILURE;
