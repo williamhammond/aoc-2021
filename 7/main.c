f5a79e533e742a39dc91bf4c267dd9c51a8d7508
William Hammond <william.t.hammond@gmail.com>
2021-12-07 21:08:37 -0500

    Day 7 part 2

--- 7/main.c
+++ 7/main.c
@@ -11,8 +11,10 @@
 #define MAX_CRAB_COUNT 2048
 #define MAX_LINE_LENGTH 8192
 
+int fuel(int n) { return (n * (n + 1)) / 2; }
+
 int main(int argc, char** argv) {
-  char* path = "test.txt";
+  char* path = "input.txt";
 
   FILE* file = fopen(path, "r");
 
@@ -24,43 +26,26 @@ int main(int argc, char** argv) {
   char line[MAX_LINE_LENGTH] = {0};
   int crabsPositions[MAX_CRAB_COUNT] = {0};
   int crabCount = 0;
+  int mean = 0;
   if (fgets(line, MAX_LINE_LENGTH, file)) {
     line[strcspn(line, "\n")] = 0;
     char* token = strtok(line, ",");
     while (token) {
       int position = atoi(token);
       crabsPositions[crabCount] = position;
+      mean += position;
       token = strtok(NULL, ",");
       crabCount++;
     }
   }
 
-  unsigned long long differences[MAX_CRAB_COUNT][MAX_CRAB_COUNT] = {0};
-  for (int i = 0; i < crabCount - 1; i++) {
-    for (int j = i + 1; j < crabCount; j++) {
-      unsigned long long cost = 0;
-      unsigned long long difference =
-          abs(crabsPositions[i] - crabsPositions[j]);
+  mean = mean / crabCount;  // NOLINT
 
-      for (unsigned long long step = 1; step <= difference; step++) {
-        cost += step;
-      }
-
-      differences[i][j] = cost;
-      differences[j][i] = cost;
-    }
-  }
-  unsigned long long answer = LLONG_MAX;
+  int sum = 0;
   for (int i = 0; i < crabCount; i++) {
-    unsigned long long sum = 0;
-    for (int j = 0; j < crabCount; j++) {
-      sum += (differences[j][i]);
-    }
-    if (sum < answer) {
-      answer = sum;
-    }
+    sum += fuel(abs(crabsPositions[i] - mean));
   }
-  printf("Answer: %llu\n", answer);
+  printf("Total fuel used: %d\n", sum);
 
   if (fclose(file)) {
     return EXIT_FAILURE;
