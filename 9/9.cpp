c27ead886a859396be0635187dcd8f9b00f2ed16
William Hammond <william.t.hammond@gmail.com>
2021-12-18 07:12:14 -0500

    Day 9 part 1 done

--- 9/9.cpp
+++ 9/9.cpp
@@ -3,11 +3,11 @@
 #include <string>
 #include <vector>
 
-static constexpr int row_size = 5;
-static constexpr int col_size = 10;
+static constexpr int row_size = 100;
+static constexpr int col_size = 100;
 static const std::string input_file = "input.txt";
 
-std::vector<std::vector<int>> getGrid()
+std::vector<std::vector<int>> get_grid()
 {
 	std::ifstream file;
 	file.open(input_file);
@@ -20,7 +20,7 @@ std::vector<std::vector<int>> getGrid()
 		for (std::string::size_type i = 0; i < col_size; i++)
 		{
 			char current = line[i];
-			grid[row_count][i] = atoi(&current); // conversion done wrong, it's doing the exact inverse
+			grid[row_count][i] = atoi(&current);
 		}
 		row_count++;
 	}
@@ -28,11 +28,10 @@ std::vector<std::vector<int>> getGrid()
 	return grid;
 }
 
-int main()
+void print_grid(const std::vector<std::vector<int>>& grid)
 {
-	const std::string input = "input.txt";
+	std::cout << "Input: " << std::endl;
 
-	const std::vector<std::vector<int>> grid = getGrid();
 	for (int i = 0; i < row_size; i++)
 	{
 		for (int j = 0; j < col_size; j++)
@@ -41,4 +40,36 @@ int main()
 		}
 		std::cout << std::endl;
 	}
+}
+
+uint64_t solve(const std::vector<std::vector<int>>& grid)
+{
+	int result = 0;
+	for (int i = 0; i < grid.size(); i++)
+	{
+		for (int j = 0; j < grid[i].size(); j++)
+		{
+			const bool is_left_higher = (j - 1 == -1) || grid[i][j - 1] > grid[i][j];
+			const bool is_right_higher = (j + 1 == grid[i].size()) || grid[i][j + 1] > grid[i][j];
+			const bool is_up_higher = (i - 1 == -1) || grid[i - 1][j] > grid[i][j];
+			const bool is_down_higher = (i + 1 == grid.size()) || grid[i + 1][j] > grid[i][j];
+			const bool is_low_point = is_left_higher && is_right_higher && is_up_higher && is_down_higher;
+
+			if (is_low_point)
+			{
+				result += grid[i][j] + 1;
+			}
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	const std::vector<std::vector<int>> grid = get_grid();
+
+	print_grid(grid);
+
+	const uint64_t result = solve(grid);
+	std::cout << "Result is: " << result << std::endl;
 }
