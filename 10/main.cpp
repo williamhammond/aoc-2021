f031b6960cf68fb78203083cb09cec3727d16468
William Hammond <william.t.hammond@gmail.com>
2021-12-18 20:21:50 -0500

    Day 10 part 2

--- 10/main.cpp
+++ 10/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -6,8 +7,10 @@
 #include <vector>
 
 static const std::string input_file = "input.txt";
-static const std::map<char, int> scores = {
+static const std::map<char, int> corruptTokenScores = {
     {')', 3}, {']', 57}, {'}', 1197}, {'>', 25137}};
+static const std::map<char, int> repairedSyntaxScores = {
+    {')', 1}, {']', 2}, {'}', 3}, {'>', 4}};
 static const std::map<char, char> tokenAssociation = {
     {'(', ')'}, {')', '('}, {'[', ']'}, {']', '['},
     {'{', '}'}, {'}', '{'}, {'<', '>'}, {'>', '<'}};
@@ -28,6 +31,55 @@ auto is_open_token(const char& token) -> bool {
   return token == '{' || token == '(' || token == '<' || token == '[';
 }
 
+auto get_repair_strings(const std::vector<std::string>& lines)
+    -> std::vector<std::string> {
+  std::vector<std::string> result;
+  for (std::string line : lines) {
+    std::stack<char> stack;
+    bool is_corrupt = false;
+    for (char token : line) {
+      if (is_open_token(token)) {
+        stack.push(token);
+      } else {
+        char closingToken = stack.top();
+        stack.pop();
+        if (tokenAssociation.at(token) != closingToken) {
+          is_corrupt = true;
+          // Skipping corrupted line
+          break;
+        }
+      }
+    }
+    if (!is_corrupt) {
+      std::string complete;
+      while (!stack.empty()) {
+        complete += tokenAssociation.at(stack.top());
+        stack.pop();
+      }
+      result.push_back(complete);
+    }
+  }
+  return result;
+}
+
+auto calc_repair_score(std::string repair_string) -> uint64_t {
+  uint64_t score = 0;
+  for (char token : repair_string) {
+    score *= 5;
+    score += repairedSyntaxScores.at(token);
+  }
+  return score;
+}
+
+auto calc_average_repair_score(std::vector<std::string> repair_strings) -> uint64_t{
+  std::vector<uint64_t> scores;
+  for (std::string repair_string : repair_strings) {
+    scores.push_back(calc_repair_score(repair_string));
+  }
+  std::sort(scores.begin(), scores.end());
+  return scores[scores.size() / 2];
+}
+
 auto calc_illegal_syntax_score(const std::vector<std::string>& lines) -> int {
   int result = 0;
   std::stack<char> stack;
@@ -39,7 +91,7 @@ auto calc_illegal_syntax_score(const std::vector<std::string>& lines) -> int {
         char closingToken = stack.top();
         stack.pop();
         if (tokenAssociation.at(token) != closingToken) {
-          result += scores.at(token);
+          result += corruptTokenScores.at(token);
           break;
         }
       }
@@ -50,6 +102,7 @@ auto calc_illegal_syntax_score(const std::vector<std::string>& lines) -> int {
 
 auto main() -> int {
   std::vector<std::string> lines = get_nav_lines();
-  int score = calc_illegal_syntax_score(lines);
+  auto repair_strings = get_repair_strings(lines);
+  auto score = calc_average_repair_score(repair_strings);
   std::cout << "Score is: " << score << std::endl;
 }
